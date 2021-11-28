13aed1df31a64843befb177c01ff8475e7abe80d
Reububble <Reububble@gmail.com>
2021-11-28 19:07:39 +1000

    permission

--- index.js
+++ index.js
@@ -7,7 +7,23 @@ function when(target, type, callback) {
 when(window, "load", function () {
     navigator.serviceWorker.register("/web/sw.js", { scope: "/web/" }).then(function (value) {
         console.log(value);
+        askPermission().then(console.log);
     })["catch"](function (reason) {
         console.error(reason);
     });
 });
+function askPermission() {
+    return new Promise(function (resolve, reject) {
+        var permissionResult = Notification.requestPermission(function (result) {
+            resolve(result);
+        });
+        if (permissionResult) {
+            permissionResult.then(resolve, reject);
+        }
+    })
+        .then(function (permissionResult) {
+        if (permissionResult !== 'granted') {
+            throw new Error('We weren\'t granted permission.');
+        }
+    });
+}
