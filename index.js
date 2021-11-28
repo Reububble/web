d09b7307413846f4383e9fb3150d739426f2c9e0
Reububble <Reububble@gmail.com>
2021-11-28 19:18:28 +1000

    register

--- index.js
+++ index.js
@@ -4,15 +4,30 @@ function when(target, type, callback) {
         stop: function () { target.removeEventListener(type, callback); }
     };
 }
-when(window, "load", function () {
-    navigator.serviceWorker.register("/web/sw.js", { scope: "/web/" }).then(function (value) {
-        console.log(value);
-    })["catch"](function (reason) {
-        console.error(reason);
-    });
-});
+function urlBase64ToUint8Array(base64String) {
+    var padding = '='.repeat((4 - base64String.length % 4) % 4);
+    var base64 = (base64String + padding)
+        .replace(/\-/g, '+')
+        .replace(/_/g, '/');
+    var rawData = window.atob(base64);
+    var outputArray = new Uint8Array(rawData.length);
+    for (var i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+}
 when(document.querySelector("button"), "click", function () {
-    askPermission().then(console.log);
+    askPermission().then(function () {
+        var subscribeOptions = {
+            userVisibleOnly: true,
+            applicationServerKey: urlBase64ToUint8Array('BEl62iUYgUivxIkv69yViEuiBIa-Ib9-SkvMeAtA3LFgDzkrxZJjSgSnfckjBJuBkr3qBUYIHBQFLXYp5Nksh8U')
+        };
+        return navigator.serviceWorker.register("/web/sw.js", { scope: "/web/" }).then(function (value) {
+            value.pushManager.subscribe(subscribeOptions);
+        })["catch"](function (reason) {
+            console.error(reason);
+        });
+    });
 });
 function askPermission() {
     return new Promise(function (resolve, reject) {
